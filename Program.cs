6c25ba881d5af489ba732b473c307f8c77d73984
Douglas Drinka <ddrinka@gmail.com>
2021-02-17 17:05:49 -0700

    Added simple repro for aws/aws-sdk-net#1118.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.S3;
+using System;
+using System.Threading.Tasks;
+
+namespace aws_sdk_1118_repro
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var s3Client = new AmazonS3Client();
+            await s3Client.ListBucketsAsync();
+            Console.WriteLine("Success");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# aws-sdk-1118-repro
+Reproduce [aws/aws-sdk-net#1118](https://github.com/aws/aws-sdk-net/issues/1118)
+
+Try to list S3 buckets with no configured AWS credentials.
